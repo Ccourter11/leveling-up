b657a577a8ff4ceb5ea64aab6285cd65360152b3
Ccourter11 <courtercalvin@yahoo.com>
2021-05-07 12:13:18 -0500

    feat: game form

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect } from "react"
 import { GameContext } from "./GameProvider.js"
+import { useHistory } from "react-router-dom"
 
 export const GameList = (props) => {
     const { games, getGames } = useContext(GameContext)
+
+    const history = useHistory()
 
     useEffect(() => {
         getGames()
@@ -16,6 +19,11 @@ export const GameList = (props) => {
                         <div className="game__title">{game.title} by {game.maker}</div>
                         <div className="game__players">{game.number_of_players} players needed</div>
                         <div className="game__skillLevel">Skill level is {game.skill_level}</div>
+                        <button className="btn btn-2 btn-sep icon-create"
+                            onClick={() => {
+                                history.push({ pathname: "/games/new" })
+                            }}
+                        >Register New Game</button>
                     </section>
                 })
             }
